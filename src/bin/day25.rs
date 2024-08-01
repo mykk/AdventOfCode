06a790f24f5505746a7e7a6369d1ad540293b901
Mykolas.Simutis <Mykolas.Simutis@bentley.com>
2024-08-01 22:32:09 +0300

    day 25

--- src/bin/day25.rs
+++ src/bin/day25.rs
@@ -1,210 +1,13 @@
 mod safe_cracking {
     use std::collections::HashMap;
+    use aoc_2016::utils::assembunny;
 
-    use once_cell::sync::Lazy;
-    use regex::Regex;
-
-    #[derive(Debug, PartialEq, Clone, Eq)]
-    pub(crate) enum Instructions {
-        Cpy(String, String),
-        Inc(String),
-        Dec(String),
-        Jnz(String, String),
-        Tgl(String),
-        Mul(String, String, String),
-        Out(String),
-        Pass(),
-    }
-
-    fn extract_capture(captures: &regex::Captures, index: usize) -> String {
-        captures.get(index).map_or_else(
-            || panic!("Capture group {} not found", index),
-            |capture| capture.as_str().to_string())
-    }
-    
-    fn parse_instruction(line: &str) -> Option::<Instructions> {
-        static PATTERNS: Lazy<Vec<Regex>> = Lazy::new(|| vec![
-            Regex::new(r"cpy (-*\w+) (\w+)").unwrap(),
-            Regex::new(r"inc (\w+)").unwrap(),
-            Regex::new(r"dec (\w+)").unwrap(),
-            Regex::new(r"jnz (-*\w+) (-*\w+)").unwrap(),
-            Regex::new(r"tgl (-*\w+)").unwrap(),
-            Regex::new(r"out (-*\w+)").unwrap(),
-        ]);
-
-        PATTERNS.iter().enumerate().find_map(|(i, pattern)| {
-            if let Some(captures) = pattern.captures(line) {
-                let instruction = match i {
-                    0 => Instructions::Cpy(extract_capture(&captures, 1), extract_capture(&captures, 2)),
-                    1 => Instructions::Inc(extract_capture(&captures, 1)),
-                    2 => Instructions::Dec(extract_capture(&captures, 1)),
-                    3 => Instructions::Jnz(extract_capture(&captures, 1), extract_capture(&captures, 2)),
-                    4 => Instructions::Tgl(extract_capture(&captures, 1)),
-                    5 => Instructions::Out(extract_capture(&captures, 1)),
-                    _ => unreachable!(),
-                };
-                return Some(instruction);
-            }
-            None
-        })
-    }
-
-    pub(crate) fn parse_assembly(lines: &[&str]) -> Vec<Instructions> {    
-        lines.iter().fold(Vec::new(), |mut vec, line| {
-            if let Some(instruction) = parse_instruction(line) {
-                vec.push(instruction);
-            }
-            else {
-                panic!("unrecognized instruction!");
-            }
-            vec
-        })
-    }
-    
-    fn get_value(values: &HashMap<String, i32>, val: &str) -> i32 {
-        if let Some(val) = val.parse().ok() {
-            val
-        }
-        else {
-            *values.get(val).unwrap_or(&0)
-        }
-    }
-
-    fn get_multiplication_result_reg<'a>(instructions: &[&'a Instructions]) -> Option<&'a String> {
-        instructions.iter().skip(3).take(2).find_map(|instruction|
-            if let Instructions::Inc(reg) = instruction {
-                Some(reg)
-            } else {
-                None
-            }
-        )
-    }
-
-    fn get_optimized_instructions(instructions: &[&Instructions]) -> Option<Vec<Instructions>> {
-        let iter = instructions.iter();
-        let init_vals = iter.clone().take(3).map(|instruction| match instruction {
-            Instructions::Cpy(val, reg) => Some((val, reg)),
-            _ => None
-        }).collect::<Option<Vec<_>>>()?;
-
-        let result_reg = get_multiplication_result_reg(instructions)?;
-
-        let mut multipliers = init_vals.iter().filter(|(_, reg)|*reg != result_reg);
-        let multiplier1 = multipliers.next()?.1;
-        let multiplier2 = multipliers.next()?.1;
-        if *multiplier1 == *multiplier2 {
-            return None;
-        }
-
-        if iter.clone().skip(3).take(2).all(|instruction| matches!(instruction, Instructions::Inc(reg) if result_reg != reg)) {
-            return None;
-        }
-
-        let current_multiplier = iter.clone().skip(3).take(2).find(|instruction| matches!(instruction, Instructions::Dec(multiplier) if multiplier == multiplier1 || multiplier == multiplier2))?;
-        let (multiplier1, multiplier2) = if matches!(current_multiplier, Instructions::Dec(multiplier) if multiplier == multiplier1) {(multiplier1, multiplier2)} else { (multiplier2, multiplier1) };
-
-        
-        let mut iter = iter.skip(5);
-        if matches!(iter.next()?, Instructions::Jnz(reg, value) if reg != multiplier1 || value != "-2") { return None; }
-        if matches!(iter.next()?, Instructions::Dec(reg) if reg != multiplier2) { return None; }
-        if matches!(iter.next()?, Instructions::Jnz(reg, value) if reg != multiplier2 || value != "-5") { return None; }
-
-        let cpy_instructions = 
-            instructions.iter().take(3).map(|x|(**x).clone()).
-            chain([Instructions::Mul(multiplier1.clone(), multiplier2.clone(), result_reg.clone())]).
-            chain([Instructions::Pass(), Instructions::Pass(), Instructions::Pass(), Instructions::Pass()]);
-        Some(cpy_instructions.collect())
-    }
-
-    fn optimize_assembly(assembly: &[Instructions]) -> Vec<Instructions> {
-        let mut index = 0;
-
-        let mut optimized_assembly = vec![];
-        while index < assembly.len() {
-            if let Some(instructions) = get_optimized_instructions(&assembly.iter().skip(index).collect::<Vec<_>>()) {
-                index += instructions.len();
-                optimized_assembly.extend(instructions.into_iter());    
-            }
-            else {
-                optimized_assembly.push(assembly[index].clone());
-                index += 1;
-            }
-        }
-
-        optimized_assembly 
-    }
-
-    pub(crate) fn execute_assembly<OutputF>(assembly: &[Instructions], initial_values: HashMap<String, i32>, mut output: OutputF) -> HashMap<String, i32> 
-    where OutputF: FnMut(&HashMap<String, i32>, i32) -> bool 
-    {
-        let mut index: usize = 0;
-        let mut values = initial_values;
-
-        let mut assembly = optimize_assembly(assembly); 
-        while index < assembly.len() {
-            index = match &assembly[index] {
-                Instructions::Cpy(value, registry) => {
-                    values.insert(registry.clone(), get_value(&values, value));
-                    index + 1    
-                }
-                Instructions::Inc(registry) => {
-                    values.entry(registry.clone()).and_modify(|value| *value += 1);
-                    index + 1
-                }
-                Instructions::Dec(registry) => {
-                    values.entry(registry.clone()).and_modify(|value| *value -= 1);
-                    index + 1
-                }
-                Instructions::Jnz(value, jump) => {
-                    if get_value(&values, value) != 0 {
-                        let jump = get_value(&values, jump);
-                        if jump < 0 { index - (-jump) as usize } else { index + jump as usize }
-                    }
-                    else {
-                        index + 1
-                    }
-                }
-                Instructions::Tgl(value) => {
-                    let value = get_value(&values, value);
-                    let tgl_index = if value < 0 { index - (-value) as usize } else { index + value as usize };
-                    if let Some(instruction) = assembly.get(tgl_index) {
-                        assembly[tgl_index] = match instruction {
-                            Instructions::Inc(val) => Instructions::Dec(val.clone()),
-                            Instructions::Dec(val) => Instructions::Inc(val.clone()),
-                            Instructions::Tgl(val) => Instructions::Inc(val.clone()),
-                            Instructions::Jnz(val1, val2) => Instructions::Cpy(val1.clone(), val2.clone()),
-                            Instructions::Cpy(val1, val2) => Instructions::Jnz(val1.clone(), val2.clone()),
-                            _ => panic!("optimized instruction toggled")
-                        }
-                    }
-                    index + 1
-                }
-                Instructions::Pass() => panic!("multiplication should jump over these intstructions and no other instruction should jump here"),
-                Instructions::Mul(val1, val2, registry) => {
-                    let original_value = get_value(&values, registry); 
-                    let val1 = get_value(&values, val1);
-                    let val2 = get_value(&values, val2);
-                    values.insert(registry.clone(), val1 * val2 + original_value);
-                    index + 5
-                }
-                Instructions::Out(val) => {
-                    let terminate = output(&values, get_value(&values, val));
-                    if terminate {
-                        return values;
-                    }
-                    index + 1
-                }
-            };
-        }
-        values
-    }
-
-    pub(crate) fn find_transmision_index(assembly: &[Instructions]) -> i32 {
+    pub(crate) fn find_transmision_index(assembly: &[assembunny::Instructions]) -> i32 {
         (0..).find(|a| {
             let mut previous_output = 1;
             let mut seen_states = Vec::new();
             let mut found_signal = false;
-            execute_assembly(assembly, [("a".to_string(), *a)].into(), |values, output_val|{
+            assembunny::execute_assembly(assembly, [("a".to_string(), *a)].into(), |values, output_val|{
                 if (output_val != 0 && output_val != 1) || (output_val == previous_output) {
                     return true;
                 }
@@ -228,12 +31,14 @@ mod safe_cracking {
 
 fn main() {
     use aoc_2016::utils::aoc_file;
-    use crate::safe_cracking::{parse_assembly, find_transmision_index};
+    use aoc_2016::utils::assembunny;
+
+    use crate::safe_cracking::find_transmision_index;
 
     let content = aoc_file::open_and_read_file(&mut std::env::args()).unwrap();
 
     let lines = content.lines().collect::<Vec<_>>();
-    let assembly = parse_assembly(&lines);
+    let assembly = assembunny::parse_assembly(&lines);
 
     let value = find_transmision_index(&assembly);
     println!("part1: {}", value);
