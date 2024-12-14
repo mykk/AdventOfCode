6d82dd74547cf2f60bd06444d3ff20482a969f43
mykk <mykolas.simutis@gmail.com>
2024-12-14 13:02:10 +0200

    day 14

---
+++ 2024/day14/main_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const EXAMPLEDATA string = `p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3`
+
+func Test(t *testing.T) {
+	t.Run("TestMovingRobots", func(t *testing.T) {
+		robots, err := ParseInputData(EXAMPLEDATA)
+		assert.Nil(t, err)
+
+		assert.Equal(t, 12, MoveRobots(robots, 100, 11, 7))
+	})
+}
