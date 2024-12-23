4ea6a392e16ffdc825f5a8f244699f20ac01b2b2
mykk <mykolas.simutis@gmail.com>
2024-12-23 22:05:07 +0100

    day 23

--- 2024/day23/main.go
+++ 2024/day23/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	fn "AoC/functional"
+
 	"AoC/aoc"
 	"fmt"
 	"os"
@@ -14,6 +16,14 @@ func spliceToSet(splice []string) aoc.Set[string] {
 		set.Add(item)
 	}
 	return set
+}
+
+func setToSplice(set aoc.Set[string]) []string {
+	splice := make([]string, 0, len(set))
+	for item := range set {
+		splice = append(splice, item)
+	}
+	return splice
 }
 
 func intersect(splice1, splice2 []string) aoc.Set[string] {
@@ -30,21 +40,22 @@ func intersect(splice1, splice2 []string) aoc.Set[string] {
 	return set
 }
 
-func CountTConnections(connections map[string][]string) (count int) {
+func CountTConnections(network map[string][]string) (count int) {
 	usedGlobal := make(aoc.Set[string])
 
-	for computer, computerConnections := range connections {
+	for computer, connections := range network {
 		if !strings.HasPrefix(computer, "t") {
 			continue
 		}
 
 		used := make(aoc.Set[string])
-		for _, connected := range computerConnections {
+		for _, connected := range connections {
 			if usedGlobal.Contains(connected) {
 				continue
 			}
 
-			interConnections := intersect(computerConnections, connections[connected])
+			interConnections := intersect(connections, network[connected])
+
 			for interConnected := range interConnections {
 				if !used.Contains(interConnected) && !usedGlobal.Contains(interConnected) {
 					count += 1
@@ -58,16 +69,58 @@ func CountTConnections(connections map[string][]string) (count int) {
 	return count
 }
 
+func getMaxClusterSize(network map[string][]string, cluster []string, seen aoc.Set[string]) []string {
+	currentAsStr := strings.Join(fn.Sorted(cluster, func(a, b string) bool { return a < b }), "")
+	if seen.Contains(currentAsStr) {
+		return []string{}
+	}
+	seen.Add(currentAsStr)
+
+	clusterConnections := network[cluster[0]]
+	for _, clusterNode := range cluster[1:] {
+		clusterConnections = setToSplice(intersect(clusterConnections, network[clusterNode]))
+	}
+
+	if len(clusterConnections) == 0 {
+		return cluster
+	}
+
+	maxCluster := cluster
+	for _, connection := range clusterConnections {
+		biggerCluster := make([]string, 0, len(cluster)+1)
+
+		current := getMaxClusterSize(network, append(append(biggerCluster, cluster...), connection), seen)
+		if len(current) > len(maxCluster) {
+			maxCluster = current
+		}
+	}
+
+	return maxCluster
+}
+
+func FindMaxCluster(network map[string][]string) []string {
+	maxCluster := make([]string, 0)
+	seen := make(aoc.Set[string])
+	for computer := range network {
+		current := getMaxClusterSize(network, []string{computer}, seen)
+		if len(current) > len(maxCluster) {
+			maxCluster = current
+		}
+	}
+
+	return fn.Sorted(maxCluster, func(a, b string) bool { return a < b })
+}
+
 func ParseInputData(data string) map[string][]string {
 	re := regexp.MustCompile(`([a-z]{2})\-([a-z]{2})`)
 	matches := re.FindAllStringSubmatch(data, -1)
 
-	connections := make(map[string][]string)
+	network := make(map[string][]string)
 	for _, match := range matches {
-		connections[match[1]] = append(connections[match[1]], match[2])
-		connections[match[2]] = append(connections[match[2]], match[1])
+		network[match[1]] = append(network[match[1]], match[2])
+		network[match[2]] = append(network[match[2]], match[1])
 	}
-	return connections
+	return network
 }
 
 func main() {
@@ -77,11 +130,12 @@ func main() {
 		return
 	}
 
-	connections := ParseInputData(string(inputData))
+	network := ParseInputData(string(inputData))
 	if err != nil {
 		fmt.Printf("Error parsing input data: %v\n", err)
 		return
 	}
 
-	fmt.Printf("Part 1: %d\n", CountTConnections(connections))
+	fmt.Printf("Part 1: %d\n", CountTConnections(network))
+	fmt.Printf("Part 2: %s\n", strings.Join(FindMaxCluster(network), ","))
 }
