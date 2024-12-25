4994e5528373c8a9f559f336713aa2b1a80cb8a6
mykk <mykolas.simutis@gmail.com>
2024-12-25 10:53:51 +0100

    day 24 part 2 wip

--- 2024/day24/main.go
+++ 2024/day24/main.go
@@ -66,16 +66,25 @@ func setToSplice(set aoc.Set[string]) []string {
 	return splice
 }
 
-func ParseInputData(data string) (map[string]int, map[string]LogicGate, []string) {
+func ParseInputData(data string) (map[string]int, map[string]LogicGate, []string, []string, []string) {
 	valueMatches := regexp.MustCompile(`(...): (1|0)`).FindAllStringSubmatch(data, -1)
 
+	xValues := make(aoc.Set[string])
+	yValues := make(aoc.Set[string])
 	zValues := make(aoc.Set[string])
 	values := make(map[string]int)
 	for _, value := range valueMatches {
 		values[value[1]] = MustAtoi(value[2])
+		if strings.HasPrefix(value[1], "x") {
+			xValues.Add(value[1])
+		}
+		if strings.HasPrefix(value[1], "y") {
+			yValues.Add(value[1])
+		}
 		if strings.HasPrefix(value[1], "z") {
 			zValues.Add(value[1])
 		}
+
 	}
 
 	connectionMatches := regexp.MustCompile(`(...) (OR|XOR|AND) (...) \-> (...)`).FindAllStringSubmatch(data, -1)
@@ -90,12 +99,19 @@ func ParseInputData(data string) (map[string]int, map[string]LogicGate, []string
 		case "XOR":
 			connections[connection[4]] = xorGate{GateOperands: GateOperands{operandA: connection[1], operandB: connection[3]}}
 		}
+		if strings.HasPrefix(connection[4], "x") {
+			xValues.Add(connection[4])
+		}
+		if strings.HasPrefix(connection[4], "y") {
+			yValues.Add(connection[4])
+		}
+
 		if strings.HasPrefix(connection[4], "z") {
 			zValues.Add(connection[4])
 		}
 	}
 
-	return values, connections, setToSplice(zValues)
+	return values, connections, setToSplice(xValues), setToSplice(yValues), setToSplice(zValues)
 }
 
 func ComputeZValue(values map[string]int, connections map[string]LogicGate, zValues []string) int {
@@ -106,6 +122,45 @@ func ComputeZValue(values map[string]int, connections map[string]LogicGate, zVal
 	})
 }
 
+func FindSwappedWires(values map[string]int, connections map[string]LogicGate, xValues, yValues, zValues []string) string {
+	xValues = fn.Sorted(xValues, func(a, b string) bool { return a < b })
+	yValues = fn.Sorted(yValues, func(a, b string) bool { return a < b })
+	zValues = fn.Sorted(zValues, func(a, b string) bool { return a < b })
+
+	fmt.Printf(" ")
+	for i := len(xValues) - 1; i != -1; i-- {
+		fmt.Printf("%d", values[xValues[i]])
+	}
+
+	fmt.Printf("\n ")
+
+	for i := len(yValues) - 1; i != -1; i-- {
+		fmt.Printf("%d", values[yValues[i]])
+	}
+	fmt.Printf("\n")
+
+	for i := len(zValues) - 1; i != -1; i-- {
+		zValue := resolveValue(values, connections, zValues[i])
+		fmt.Printf("%d", zValue)
+	}
+	fmt.Printf("\n")
+
+	carryOver := 0
+	for i, xValue := range xValues {
+		result := values[xValue] + values[yValues[i]] + carryOver
+
+		resultValue := result % 2
+		carryOver = result / 2
+
+		zValue := resolveValue(values, connections, zValues[i])
+		if resultValue != zValue {
+			fmt.Printf("SusBit: %d\n", i+1)
+		}
+	}
+
+	return ""
+}
+
 func main() {
 	inputData, err := os.ReadFile("day24.txt")
 	if err != nil {
@@ -113,7 +168,8 @@ func main() {
 		return
 	}
 
-	values, connections, zValues := ParseInputData(string(inputData))
+	values, connections, xValues, yValues, zValues := ParseInputData(string(inputData))
 
 	fmt.Printf("Part 1: %d\n", ComputeZValue(values, connections, zValues))
+	fmt.Printf("Part 2: %s\n", FindSwappedWires(values, connections, xValues, yValues, zValues))
 }
